8d56361fd3a2177c6d72a19220cf8ed755da8116
Jason Trader <jasonmtrader@gmail.com>
2016-03-15 17:14:23 -0700

    Changed input file to not have commas

--- createInput.py
+++ createInput.py
@@ -5,9 +5,9 @@ for pid in range (0, 10000):
     cpu_to_io_count = random.randint(0, 5)
     cpu_to_io_count *= 2
     cpu_to_io_count += 1
-    f.write(str(pid) + ', Arrival ' + str(arrival_time))
+    f.write(str(pid) + ' Arrival ' + str(arrival_time))
     for i in range (0, cpu_to_io_count):
         time = random.randint(1, 100)
-        f.write(', IO ') if i%2 else f.write(', CPU ')
+        f.write(' IO ') if i%2 else f.write(' CPU ')
         f.write(str(time))
     f.write('\n')
