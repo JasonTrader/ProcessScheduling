b31b437f2d74cff57ec6431791322aafebe3ecf6
JasonTrader <jasonmtrader@gmail.com>
2016-03-16 15:31:52 -0700

    Made PCB constructor

--- SimulationFiles/PCB.h
+++ SimulationFiles/PCB.h
@@ -5,13 +5,22 @@
 #include <vector>
 #include "Core.h"
 
+enum SCHED_TYPE { FCFS, RR, SPN, SRT, HRRN, Feedback};
+
 class PCB {
 private:
-	unsigned int m_completed_processes = 0;
-	unsigned int m_num_context_switch = 0;
-	enum schedule_type { FCFS, RR, SPN, SRT, HRRN, Feedback};
+	unsigned int completed_processes = 0;
+	unsigned int num_context_switch = 0;
+	unsigned int sim_time;
+	vector<Process> procs;
+	SCHED_TYPE sched_string;
 
 public:
+	PCB(vector<Process> procs, unsigned int sim_time, SCHED_TYPE sched_string){
+		this.procs = procs;
+		this.sim_time = sim_time;
+		this.sched_string = sched_string;
+	}
 };
 
 #endif
