6885433f67b450639a55926397ce5234739f498c
Jason Trader <jasonmtrader@gmail.com>
2016-03-15 18:49:49 -0700

    Started Work on PCB

--- SimulationFiles/PCB.h
+++ SimulationFiles/PCB.h
@@ -1,14 +1,17 @@
 #ifndef __PCB_H__
 #define __PCB_H__
 
-class ProcessControlBlock {
+#include <queue>
+#include <vector>
+#include "Core.h"
+
+class PCB {
 private:
 	unsigned int m_completed_processes = 0;
-
-protected:
+	unsigned int m_num_context_switch = 0;
+	enum schedule_type { FCFS, RR, SPN, SRT, HRRN, Feedback};
 
 public:
-
 };
 
 #endif
