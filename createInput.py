a8e44fbf159bb06ebaa1eeab3b24547e4ebc0d56
Jason Trader <jasonmtrader@gmail.com>
2016-03-18 09:27:41 -0700

    Updated input script

--- createInput.py
+++ createInput.py
@@ -1,13 +1,17 @@
 import random
+import sys
 f = open('data.txt', 'w')
-for pid in range (0, 10000):
-    arrival_time = random.randint(0, 100000)
+process_num = sys.argv[1]
+max_burst_time = sys.argv[2]
+total_run_time = sys.argv[3]
+for pid in range (0, int(process_num)):
+    arrival_time = random.randint(0, int(total_run_time))
     cpu_to_io_count = random.randint(0, 5)
     cpu_to_io_count *= 2
     cpu_to_io_count += 1
     f.write(str(pid) + ' Arrival ' + str(arrival_time))
     for i in range (0, cpu_to_io_count):
-        time = random.randint(1, 100)
+        time = random.randint(1, int(max_burst_time))
         f.write(' IO ') if i%2 else f.write(' CPU ')
         f.write(str(time))
     f.write('\n')
