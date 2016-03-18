ee4916fa6122b27cfeaa39a338e56f3bc2599db6
Jason Trader <jasonmtrader@gmail.com>
2016-03-18 10:00:29 -0700

    Updated core to have unused time count

--- SimulationFiles/Core.h
+++ SimulationFiles/Core.h
@@ -6,6 +6,7 @@ class Core {
 private:
 	Process *m_curr_process;
 	bool m_needs_process;
+	unsigned int unused_count = 0;
 	//bool m_process_preempted;
 public:
 	Core Core() {
@@ -21,20 +22,26 @@ public:
 	//runs one clock tick for a process argument should be passed as NULL if replacement is not desired
 	Process Update(Process &p) {
 		//new process in, used for pre-emption/replacement
-		if(*p != NULL) {
+		if(p != NULL) {
 			m_curr_process = p;
 			m_needs_process = false;
 			return m_curr_process;
 		}
+		if(m_curr_process == NULL){
+			unused_count++;
+			return NULL;
+		}
 		//decrement burst time
-		if(m_curr_process.getBurstTimeLeft() > 0) {
-				m_curr_process.decBurstTimeLeft();
+		if( m_curr_process->getBurstTimeLeft() > 0) {
+				m_curr_process->decBurstTimeLeft();
 			}
 		//remove from core if process finishes cpu burst
-		if (m_curr_process.getBurstTimeLeft() == 0) {
-			m_curr_process.removeFrontBurst();
+		if (m_curr_process->getBurstTimeLeft() == 0) {
+			m_curr_process->removeFrontBurst();
 			m_needs_process = true;
-			return m_curr_process;
+			Process *temp = m_curr_process;
+			m_curr_process = NULL;
+			return temp;
 		}
 		return NULL;
 	}
