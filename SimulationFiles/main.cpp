2e09a596fdd49985fb338595e31cd1e8e5ba7794
Jason Trader <jasonmtrader@gmail.com>
2016-03-29 18:38:35 -0700

    Changed sort

--- SimulationFiles/main.cpp
+++ SimulationFiles/main.cpp
@@ -6,6 +6,8 @@ int main() {
   //put set up here
   PCB pcb = PCB(procs, sim_time, sched_string, cores);
 
+  sort(unarrived_procs->begin(), unarrived_procs->end());
+
   for (int clock_int = 0; clock_int < pcb.getSimTime(); clock_int++) {
     pcb.Update(clock_int);
   }
