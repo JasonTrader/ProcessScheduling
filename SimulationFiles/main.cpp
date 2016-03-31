ec12ea2efb28283941da18df4477fb9bfa1fbd86
Patrick <patricktrherbert@gmail.com>
2016-03-31 16:44:02 -0700

    main.cpp now reads from data file and creates PCB

--- SimulationFiles/main.cpp
+++ SimulationFiles/main.cpp
@@ -3,14 +3,45 @@
 using namespace std;
 
 int main() {
-  //put set up here
-  PCB pcb = PCB(procs, sim_time, sched_string, cores);
+	vector<Process> procs;
+	//hard coded for now
+	unsigned int sim_time = 100000;
+	SCHED_TYPE sched_string = FCFS;
+	unsigned int core_num = 1;
+	unsigned int quantum = 20;
+	bool uniproc = true;
 
-  sort(unarrived_procs->begin(), unarrived_procs->end());
+	string line;
+	ifstream myfile("data.txt");
+	if (myfile.is_open()) {
+		while (getline(myfile, line)) {
+			stringstream ss(line);
+			istream_iterator<string> begin(ss);
+			istream_iterator<string> end;
+			vector<string> vstrings(begin, end);
+			
+			vector<pair<string, unsigned int>> bursts;
+			for (int i = 5; i < vstrings.size(); i += 2) {
+				if (vstrings[i - 1].compare("CPU")) {
+					bursts.push_back(<"CPU", atoi(vstrings[i].c_str()>));
+				}
+				else {
+					bursts.push_back(<"IO", atoi(vstrings[i].c_str()>));
+				}
+			}
 
-  for (int clock_int = 0; clock_int < pcb.getSimTime(); clock_int++) {
-    pcb.Update(clock_int);
-  }
+			procs.push_back(new Process(vstrings[0], vstrings[2], burts));
+		}
+		myfile.close();
+	}
+	else cout << "Unable to open data file :(" << endl;
+	
+	PCB pcb = PCB(procs, sim_time, sched_string, core_num, quantum, uniproc);
 
-  return 0;
+	//start running simulation
+	for (int clock_int = 0; clock_int < pcb.getSimTime(); clock_int++) {
+		pcb.Update(clock_int);
+	}
+
+	return 0;
 }
