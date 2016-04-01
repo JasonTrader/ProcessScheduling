c22b09bc4e6445841f406601f5069e67838bc932
Patrick <patricktrherbert@gmail.com>
2016-03-31 17:58:18 -0700

    Fixed syntax, constructor, added default constructor

--- SimulationFiles/Process.h
+++ SimulationFiles/Process.h
@@ -3,100 +3,108 @@
 
 #include <vector>
 #include <utility>
+#include <string>
 
-class Process{
+class Process {
 private:
-  int PID;
-  unsigned int arriv_time;
-  unsigned int tot_burst_time;
-  unsigned int response_time;
-  bool responded_to;
-  std::vector<std::pair<string, unsigned int>> bursts;
+	unsigned int PID;
+	unsigned int arriv_time;
+	unsigned int tot_burst_time;
+	unsigned int response_time;
+	bool responded_to;
+	std::vector<std::pair<std::string, unsigned int>> bursts;
 
 public:
 
-  Process(int PID, unsigned int arriv_time, std::vector<std::pair<string, unsigned int>> bursts){
-    tot_burst_time = 0;
-    for(int i = 0; i<bursts.size(); i++){
-      tot_burst_time += bursts[i].second;
-    }
-  }
+	Process(unsigned int _PID, unsigned int _arriv_time, std::vector<std::pair<std::string, unsigned int>> _bursts) {
+		PID = _PID;
+		arriv_time = _arriv_time;
+		bursts = _bursts;
+		tot_burst_time = 0;
+		for (unsigned int i = 0; i < bursts.size(); i++) {
+			tot_burst_time += bursts[i].second;
+		}
+	}
 
-  bool operator<(const Process &other) {//compares on arrival time
-    return arriv_time < other.arriv_time;
-  }
+	Process() {
 
-  unsigned int getArrivalTime() {
-    return arriv_time;
-  }
-  unsigned int getTurnaroundTime(unsigned int clockTime){
-    return clockTime - arriv_time;
-  }
+	}
 
-  unsigned int getTotBurst(){
-    return tot_burst_time;
-  }
+	bool operator<(const Process &other) {//compares on arrival time
+		return arriv_time < other.arriv_time;
+	}
+
+	unsigned int getArrivalTime() {
+		return arriv_time;
+	}
+	unsigned int getTurnaroundTime(unsigned int clockTime) {
+		return clockTime - arriv_time;
+	}
+
+	unsigned int getTotBurst() {
+		return tot_burst_time;
+	}
 
 #pragma region core functions
-  unsigned int getBurstTimeLeft(){
-    if(!bursts.empty()){
-      return bursts[0].second;
-    }
-  }
+	unsigned int getBurstTimeLeft() {
+		if (!bursts.empty()) {
+			return bursts[0].second;
+		}
+	}
 
-  void decBurstTimeLeft(){
-    if(!bursts.empty()){
-      bursts[0].second -= 1;
-    }
-  }
+	void decBurstTimeLeft() {
+		if (!bursts.empty()) {
+			bursts[0].second -= 1;
+		}
+	}
 
-  void removeFrontBurst(){
-    if(!bursts.empty()){
-      bursts.erase(bursts.begin());
-    }
-  }
+	void removeFrontBurst() {
+		if (!bursts.empty()) {
+			bursts.erase(bursts.begin());
+		}
+	}
 
-  bool isDone(){
-    return bursts.empty();
-  }
+	bool isDone() {
+		return bursts.empty();
+	}
 
 #pragma endregion
 
-  bool isWaitingIO() {
-    if (burst[0].first == "IO" && burst[0].second <= 0) {
-      bursts.erase(bursts.begin());
-      return false;
-    }
-    else if (bursts[0].first == "IO")
-      return true;
-    return false;
-  }
+	bool isWaitingIO() {
+		if (bursts[0].first == "IO" && bursts[0].second <= 0) {
+			bursts.erase(bursts.begin());
+			return false;
+		}
+		else if (bursts[0].first == "IO")
+			return true;
+		return false;
+	}
 
 
-  unsigned int getWaitTime(unsigned int clockTime){
-    return (clockTime - arriv_time) - tot_burst_time
-  }
+	unsigned int getWaitTime(unsigned int clockTime) {
+		return (clockTime - arriv_time) - tot_burst_time;
+	}
 
-  unsigned int getResponseTime(){
-    return response_time;
-  }
+	unsigned int getResponseTime() {
+		return response_time;
+	}
 
-  void setResponseTime(unsigned int clockTime){
-    if(!responded_to){
-      responded_to = true;
-      response_time = (clockTime - arriv_time);
-    }
-  }
+	void setResponseTime(unsigned int clockTime) {
+		if (!responded_to) {
+			responded_to = true;
+			response_time = (clockTime - arriv_time);
+		}
+	}
 
-  bool hasBurstTimeLeft(){
-    if(bursts.empty()){
-      return false;
-    }
-    if(bursts[0].second > 0){
-      return true;
-    }
-    return false;
-  }
+	bool hasBurstTimeLeft() {
+		if (bursts.empty()) {
+			return false;
+		}
+		if (bursts[0].second > 0) {
+			return true;
+		}
+		return false;
+	}
 
 
 };
