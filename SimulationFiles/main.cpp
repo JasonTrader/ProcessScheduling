9da803884d229b92faacddbf88e59031692ead5a
Patrick <patricktrherbert@gmail.com>
2016-03-31 17:59:46 -0700

    Main.cpp runs now! Yay :-)

--- SimulationFiles/main.cpp
+++ SimulationFiles/main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 #include "PCB.h"
+#include <iterator>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
 using namespace std;
 
 int main() {
@@ -15,22 +20,26 @@ int main() {
 	ifstream myfile("data.txt");
 	if (myfile.is_open()) {
 		while (getline(myfile, line)) {
-			stringstream ss(line);
+			istringstream ss(line);
 			istream_iterator<string> begin(ss);
 			istream_iterator<string> end;
 			vector<string> vstrings(begin, end);
 			
 			vector<pair<string, unsigned int>> bursts;
-			for (int i = 5; i < vstrings.size(); i += 2) {
-				if (vstrings[i - 1].compare("CPU")) {
-					bursts.push_back(<"CPU", atoi(vstrings[i].c_str()>));
+			unsigned int PID = atoi(vstrings[0].c_str());
+			unsigned int arrival_time = atoi(vstrings[2].c_str());
+
+			for (unsigned int i = 4; i < vstrings.size(); i += 2) {
+				if (vstrings[i - 1].compare("CPU") == 0) {
+					bursts.push_back(make_pair("CPU", atoi(vstrings[i].c_str())));
 				}
 				else {
-					bursts.push_back(<"IO", atoi(vstrings[i].c_str()>));
+					bursts.push_back(make_pair("IO", atoi(vstrings[i].c_str())));
 				}
 			}
 
-			procs.push_back(new Process(vstrings[0], vstrings[2], burts));
+			Process *p = new Process(PID, arrival_time, bursts);
+			procs.push_back(*p);
 		}
 		myfile.close();
 	}
@@ -39,8 +48,10 @@ int main() {
 	PCB pcb = PCB(procs, sim_time, sched_string, core_num, quantum, uniproc);
 
 	//start running simulation
-	for (int clock_int = 0; clock_int < pcb.getSimTime(); clock_int++) {
+	for (unsigned int clock_int = 0; clock_int < pcb.getSimTime(); clock_int++) {
 		pcb.Update(clock_int);
+		if (clock_int % 1000 == 0) 
+			cout << "At clock int: " << clock_int << endl;
 	}
 
 	return 0;
