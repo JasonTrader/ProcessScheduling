ee48f0c9b321cb4e0603f3ee7748a8137801a3b5
Patrick <patricktrherbert@gmail.com>
2016-03-31 17:59:17 -0700

    Fixed pointer problems, finished RR, added SPN & SRT*
    *SRT does not currently work

--- SimulationFiles/GetNext.h
+++ SimulationFiles/GetNext.h
@@ -1,57 +1,63 @@
 #ifndef __GETNEXT_H__
 #define __GETNEXT_H__
+#include <iostream>
 #include "Core.h"
 
-enum SCHED_TYPE { FCFS, RR, SPN, SRT, Feedback};
+enum SCHED_TYPE { FCFS, RR, SPN, SRT, Feedback };
 
-Process getNext(std::vector<Process> &procs_in_use, SCHED_TYPE sched, Core *core, unsigned int quantum){
-  if(procs_in_use.empty()){
-    return NULL;
-  }
-  switch(sched){
-    case FCFS:
-      if(core->needsProcess()){
-        return procs_in_use[0];
-      }
-      return NULL;
+Process* getNext(std::vector<Process> &procs_in_use, SCHED_TYPE sched, Core *core, unsigned int quantum) {
+	if (procs_in_use.empty()) {
+		return NULL;
+	}
+	switch (sched) {
+	case FCFS:
+		if (core->needsProcess()) {
+			Process *p = new Process();
+			*p = procs_in_use[0];
+			return p;
+		}
+		return NULL;
+		break;
 
-    case RR:
-      unsigned int curr_used = core->getCurrRunTime();
-      if(curr_used < quantum && !(core->needsProcess())) {
-        return NULL;
-      }
-      return procs_in_use[0];
+	case RR:
+		if (core->getCurrRunTime() < quantum && !(core->needsProcess())) {
+			return NULL;
+		}
+		return &procs_in_use[0];
+		break;
 
-    case SPN:
-      if (core->needsProcess()){
-        Process next_to_run = new Process();
-        for (int i = 0; i < procs_in_use->size(); i++) {
-          if (i == 0)
-            next_to_run = procs_in_use[i];
-          if (next_to_run.getBurstTimeLeft() > procs_in_use[i]->getBurstTimeLeft()) {
-            next_to_run = procs_in_use[i];
-          }
-        }
-        return next_to_run;
-      }
-      return NULL;
+	case SPN:
+		if (core->needsProcess()) {
+			Process *next_to_run = new Process();
+			for (unsigned int i = 0; i < procs_in_use.size(); i++) {
+				if (i == 0)
+					next_to_run = &procs_in_use[i];
+				if (next_to_run->getBurstTimeLeft() > procs_in_use[i].getBurstTimeLeft()) {
+					next_to_run = &procs_in_use[i];
+				}
+			}
+			return next_to_run;
+		}
+		return NULL;
+		break;
 
-    case SRT: 
-      Process next_to_run = new Process();
-      for (int i = 0; i < procs_in_use->size(); i++) {
-        if (i == 0)
-          next_to_run = procs_in_use[i];
-        if (next_to_run.getBurstTimeLeft() > procs_in_use[i]->getBurstTimeLeft()) {
-           next_to_run = procs_in_use[i];
-        }
-      }
-      return next_to_run;
-      
-      return NULL;
-    case default:
-      cout << "We Messed up somewhere" << endl;
-      return NULL;
-  }
+	case SRT: // DOES NOT WORK RIGHT
+	{
+		Process *next_to_run = new Process();
+		for (unsigned int i = 0; i < procs_in_use.size(); i++) {
+			if (i == 0)
+				next_to_run = &procs_in_use[i];
+			if (next_to_run->getBurstTimeLeft() > procs_in_use[i].getBurstTimeLeft()) {
+				next_to_run = &procs_in_use[i];
+			}
+		}
+		return next_to_run;
+	}
+	default:
+		std::cout << "We Messed up somewhere" << std::endl;
+		return NULL;
+		break;
+	}
 }
 
 #endif
